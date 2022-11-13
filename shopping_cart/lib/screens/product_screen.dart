9840744f460d7576eb6ab667f70858f31d8691f8
sohag-84 <injamulhaqsohag19977@gmail.com>
2022-11-13 22:46:05 +0600

    product screen is completed

--- shopping_cart/lib/screens/product_screen.dart
+++ shopping_cart/lib/screens/product_screen.dart
@@ -1,9 +1,132 @@
+// ignore_for_file: prefer_const_constructors, prefer_interpolation_to_compose_strings
+
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+
 class ProductListScreen extends StatelessWidget {
-  const ProductListScreen({Key? key}) : super(key: key);
+  ProductListScreen({Key? key}) : super(key: key);
+
+  List<String> productName = [
+    'Banana',
+    'Boiler',
+    'Carrot',
+    'Chili',
+    'Egg',
+    'Mutton',
+    'Oil',
+  ];
+  List<String> productUnit = [
+    'Dozen',
+    'KG',
+    'KG',
+    'KG',
+    'Dozen',
+    'KG',
+    'KG',
+  ];
+  List<int> productPrice = [10, 20, 30, 40, 50, 60, 70];
+  List<String> productImage = [
+    'images/banana.png',
+    'images/boiler_chicken.png',
+    'images/carrot.png',
+    'images/chili.png',
+    'images/egg.png',
+    'images/mutton.png',
+    'images/oil.jpg',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Product list"),
+        centerTitle: true,
+        actions: [
+          Center(
+            child: Badge(
+              badgeContent: Text(
+                "0",
+                style: TextStyle(color: Colors.white),
+              ),
+              child: Icon(Icons.shopping_bag_outlined),
+            ),
+          ),
+          SizedBox(width: 20)
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: productImage.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Image.asset(
+                            productImage[index],
+                            height: 100,
+                            width: 100,
+                          ),
+                          SizedBox(width: 10),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                productName[index],
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                productUnit[index] +
+                                    " " +
+                                    r"$" +
+                                    productPrice[index].toString(),
+                              ),
+                              SizedBox(height: 5),
+                              InkWell(
+                                onTap: () {},
+                                child: Container(
+                                  height: 35,
+                                  width: 120,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    color: Colors.green,
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "Add to cart",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 17,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
